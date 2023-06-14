0b26fc3c4a508744886089f0189129cba97adebf
Auliaintan28 <113759249+Auliaintan28@users.noreply.github.com>
2023-06-14 13:16:16 +0800

    Add files via upload

---
+++ lumayanlambat.py
@@ -0,0 +1,25 @@
+print("Aulia Intan Prasasti")
+import time
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            # Menambahkan delay setiap pertukaran elemen
+            time.sleep(0.1)
+
+# Input data dari pengguna
+data = input("Masukkan angka-angka yang ingin diurutkan, pisahkan dengan spasi: ")
+data = list(map(int, data.split()))
+
+# Measure execution time
+start_time = time.time()
+bubble_sort(data)
+end_time = time.time()
+execution_time = end_time - start_time
+
+print("Data Terurut:")
+print(data)
+print("Execution Time:", execution_time, "seconds")
